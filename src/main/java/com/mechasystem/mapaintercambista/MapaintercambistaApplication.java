1c0a98e8137c682fce72199745b75f31f4a1c548
Pablo Emanuel <emanuelpabloferreira1@gmail.com>
2026-03-23 19:12:31 -0300

    Projeto criado com dependências: Flyway, MySQL Driver, JPA, Spring Security, Spring Web, Lombok, Validation

---
+++ src/main/java/com/mechasystem/mapaintercambista/MapaintercambistaApplication.java
@@ -0,0 +1,13 @@
+package com.mechasystem.mapaintercambista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapaintercambistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapaintercambistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mechasystem/mapaintercambista/MapaintercambistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mechasystem.mapaintercambista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapaintercambistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
