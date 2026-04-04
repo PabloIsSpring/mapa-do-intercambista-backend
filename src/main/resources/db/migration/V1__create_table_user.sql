662b5692932a5637448b8c1288b73163ed3ca7fe
Pablo Emanuel <emanuelpabloferreira1@gmail.com>
2026-04-04 16:21:02 -0300

    updt: incrementação da tabela de intercambistas

--- src/main/resources/db/migration/V1__create_table_user.sql
+++ src/main/resources/db/migration/V1__create_table_user.sql
@@ -5,3 +5,13 @@ CREATE TABLE users (
     role VARCHAR (50) NOT NULL,
     deletedAt DATE
 );
+
+CREATE TABLE intercambistas (
+    id CHAR (36) primary key,
+    id_user CHAR() NOT NULL,
+    nome VARCHAR(60),
+    sobrenome VARCHAR(60),
+    username VARCHAR(50),
+    idade int,
+    FOREIGN KEY (id_user) REFERENCES users(id)
+);
