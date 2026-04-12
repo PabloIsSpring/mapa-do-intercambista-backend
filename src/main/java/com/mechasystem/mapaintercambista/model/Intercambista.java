426e09c1ef38e5a9acf37cc8d632e3fcca0b970f
Pablo Emanuel <emanuelpabloferreira1@gmail.com>
2026-04-12 17:45:18 -0300

    Classe model e classe repository de intercambista feitas

---
+++ src/main/java/com/mechasystem/mapaintercambista/model/Intercambista.java
@@ -0,0 +1,33 @@
+package com.mechasystem.mapaintercambista.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "intercambistas")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+
+public class Intercambista {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @OneToOne
+    @JoinColumn(name = "id_user", nullable = false, unique = true)
+    private User user;
+
+    @Column(unique = true  )
+    private String username;
+
+    private String nome;
+    private String sobrenome;
+    private int idade;
+
+}
---
+++ src/main/java/com/mechasystem/mapaintercambista/repository/IntercambistaRepository.java
@@ -0,0 +1,14 @@
+package com.mechasystem.mapaintercambista.repository;
+
+import com.mechasystem.mapaintercambista.model.Intercambista;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface IntercambistaRepository extends JpaRepository<Intercambista, UUID> {
+
+    public Optional<Intercambista> findByUsername(String username);
+}
