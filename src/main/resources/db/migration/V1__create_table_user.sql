681d6a7a298f05bab32345d84572645afa739fc9
Pablo Emanuel <emanuelpabloferreira1@gmail.com>
2026-04-13 19:10:11 -0300

    Update no arquivo de migration do banco com os usuários principais

--- src/main/resources/db/migration/V1__create_table_user.sql
+++ src/main/resources/db/migration/V1__create_table_user.sql
@@ -3,15 +3,15 @@ CREATE TABLE users (
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role VARCHAR (50) NOT NULL,
-    deletedAt DATE
+    deleted_at DATE
 );
 
 CREATE TABLE intercambistas (
     id CHAR (36) primary key,
-    id_user CHAR() NOT NULL,
-    nome VARCHAR(60),
-    sobrenome VARCHAR(60),
-    username VARCHAR(50),
+    id_user CHAR(36) NOT NULL UNIQUE,
+    nome VARCHAR(255),
+    sobrenome VARCHAR(255),
+    username VARCHAR(255) UNIQUE,
     idade int,
     FOREIGN KEY (id_user) REFERENCES users(id)
 );
