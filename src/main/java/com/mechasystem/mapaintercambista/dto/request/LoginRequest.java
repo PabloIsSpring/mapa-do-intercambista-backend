341d7b0dbc83dd18d7b5c05300fe33f77ae0749c
Pablo Emanuel <emanuelpabloferreira1@gmail.com>
2026-04-13 18:54:07 -0300

    Update nos DTOS de RegisterUser e LoginRequest com validator de e-mail

--- src/main/java/com/mechasystem/mapaintercambista/dto/request/LoginRequest.java
+++ src/main/java/com/mechasystem/mapaintercambista/dto/request/LoginRequest.java
@@ -1,6 +1,5 @@
 package com.mechasystem.mapaintercambista.dto.request;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 
 public record LoginRequest(@NotEmpty(message = "E-mail é obrigatório") String email,
--- src/main/java/com/mechasystem/mapaintercambista/dto/request/RegisterUserRequest.java
+++ src/main/java/com/mechasystem/mapaintercambista/dto/request/RegisterUserRequest.java
@@ -1,8 +1,14 @@
 package com.mechasystem.mapaintercambista.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 
-public record RegisterUserRequest(@NotEmpty(message = "Nome é obrigatório") String nome,
-                                  @NotEmpty(message = "E-mail é obrigatório") String email,
-                                  @NotEmpty(message = "Senha é obrigatório") String password) {
+public record RegisterUserRequest(
+        @NotEmpty(message = "Nome é obrigatório") String nome,
+        @NotEmpty(message = "E-mail é obrigatório") @Email String email,
+        @NotEmpty(message = "Senha é obrigatório") String password,
+        @NotEmpty(message = "Username é obrigatório") String username,
+        @NotEmpty(message = "Username é obrigatório") String sobrenome,
+        @Min(0) int idade) {
 }
