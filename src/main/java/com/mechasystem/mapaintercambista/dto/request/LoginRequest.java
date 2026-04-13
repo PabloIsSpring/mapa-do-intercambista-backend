ca9fe9a6ae9f5f10c0b1665e27208e1292cba5d5
Pablo Emanuel <emanuelpabloferreira1@gmail.com>
2026-04-13 18:55:29 -0300

    Update nos DTOS de RegisterUser e LoginRequest com validator de e-mail

--- src/main/java/com/mechasystem/mapaintercambista/dto/request/LoginRequest.java
+++ src/main/java/com/mechasystem/mapaintercambista/dto/request/LoginRequest.java
@@ -1,7 +1,8 @@
 package com.mechasystem.mapaintercambista.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 
-public record LoginRequest(@NotEmpty(message = "E-mail é obrigatório") String email,
+public record LoginRequest(@NotEmpty(message = "E-mail é obrigatório") @Email String email,
                            @NotEmpty(message = "Senha é obrigatório") String password) {
 }
