3722468d080381a33fc7215e25cedd9e63bd9ec0
Pablo Emanuel <emanuelpabloferreira1@gmail.com>
2026-04-13 19:20:58 -0300

    Update no UserService para registrar intercambista também

--- src/main/java/com/mechasystem/mapaintercambista/service/UserService.java
+++ src/main/java/com/mechasystem/mapaintercambista/service/UserService.java
@@ -3,27 +3,37 @@ package com.mechasystem.mapaintercambista.service;
 import com.mechasystem.mapaintercambista.config.TokenConfig;
 import com.mechasystem.mapaintercambista.dto.request.LoginRequest;
 import com.mechasystem.mapaintercambista.dto.request.RegisterUserRequest;
+import com.mechasystem.mapaintercambista.dto.response.IntercambistaResponse;
 import com.mechasystem.mapaintercambista.dto.response.LoginResponse;
 import com.mechasystem.mapaintercambista.dto.response.RegisterUserResponse;
 import com.mechasystem.mapaintercambista.enums.Role;
+import com.mechasystem.mapaintercambista.model.Intercambista;
 import com.mechasystem.mapaintercambista.model.User;
+import com.mechasystem.mapaintercambista.repository.IntercambistaRepository;
 import com.mechasystem.mapaintercambista.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final IntercambistaService intercambistaService;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final TokenConfig tokenConfig;
 
-    public UserService (UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, TokenConfig tokenConfig) {
+    public UserService (UserRepository userRepository, PasswordEncoder passwordEncoder,
+                        AuthenticationManager authenticationManager, TokenConfig tokenConfig,
+                        IntercambistaService intercambistaService) {
         this.userRepository = userRepository;
+        this.intercambistaService = intercambistaService;
         this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
         this.tokenConfig = tokenConfig;
@@ -40,7 +50,12 @@ public class UserService {
         return new LoginResponse(token);
     }
 
+    @Transactional
     public RegisterUserResponse registerUser(RegisterUserRequest request) {
+        if(userRepository.findUserByEmail(request.email()).isPresent()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "E-mail já está em uso");
+        }
+
         User nUser = new User();
         nUser.setRole(Role.ROLE_USER);
         nUser.setEmail(request.email());
@@ -48,8 +63,15 @@ public class UserService {
 
         userRepository.save(nUser);
 
+        IntercambistaResponse intercambistaResponse = intercambistaService.saveIntercambista(request, nUser);
 
-        return new RegisterUserResponse(nUser.getEmail());
+        return new RegisterUserResponse(
+                nUser.getEmail(),
+                intercambistaResponse.username()
+        );
     }
 
+    public RegisterUserResponse registerAgencia(RegisterUserRequest request) {
+        return null;
+    }
 }
