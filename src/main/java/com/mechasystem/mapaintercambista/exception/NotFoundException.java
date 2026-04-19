186bf8b00d02ff0bcd77e67f53108dbb49eac8f1
Pablo Ferreira <emanuelpabloferreira1@gmail.com>
2026-04-19 17:20:33 -0300

    Classes para exceptions do sistema

---
+++ src/main/java/com/mechasystem/mapaintercambista/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.mechasystem.mapaintercambista.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
