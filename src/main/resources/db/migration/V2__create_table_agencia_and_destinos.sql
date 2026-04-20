0291097cc817fcdcd1bfdb29162ec9261227856d
Pablo Ferreira <emanuelpabloferreira1@gmail.com>
2026-04-20 15:53:19 -0300

    Entidades de agência, paises e destinos implementados na V2 do banco de dados

---
+++ src/main/resources/db/migration/V2__create_table_agencia_and_destinos.sql
@@ -0,0 +1,27 @@
+CREATE TABLE agencias (
+    id CHAR(36) PRIMARY KEY,
+    id_user CHAR(36) NOT NULL UNIQUE,
+    razao_social VARCHAR(255) UNIQUE,
+    nome_fantasia VARCHAR (255),
+    username VARCHAR(255) UNIQUE,
+    cnpj VARCHAR(18) UNIQUE,
+    FOREIGN KEY (id_user) REFERENCES users(id)
+);
+
+CREATE TABLE paises (
+    id CHAR(2) PRIMARY KEY,
+    nome VARCHAR(100),
+    idioma_principal VARCHAR(30) NOT NULL,
+    moeda VARCHAR(30)
+);
+
+CREATE TABLE destinos (
+    id CHAR(36) PRIMARY KEY,
+    id_agencia CHAR(36) NOT NULL,
+    id_pais CHAR(2) NOT NULL,
+    cidade VARCHAR(60) NOT NULL,
+    universidade VARCHAR(60) NOT NULL,
+    descricao TEXT,
+    FOREIGN KEY (id_agencia) REFERENCES agencias(id),
+    FOREIGN KEY (id_pais) REFERENCES paises(id)
+);
