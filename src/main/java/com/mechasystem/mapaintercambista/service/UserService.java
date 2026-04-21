1093f129bfb9d262eddbc9c7bc6ac075477db986
Pablo Ferreira <emanuelpabloferreira1@gmail.com>
2026-04-21 13:49:58 -0300

    UserService atendendo aos registros de Intercambistas e Agencia

--- src/main/java/com/mechasystem/mapaintercambista/service/UserService.java
+++ src/main/java/com/mechasystem/mapaintercambista/service/UserService.java
@@ -1,40 +1,40 @@
 package com.mechasystem.mapaintercambista.service;
 
 import com.mechasystem.mapaintercambista.config.TokenConfig;
+import com.mechasystem.mapaintercambista.dto.request.CreateAgenciaRequest;
 import com.mechasystem.mapaintercambista.dto.request.LoginRequest;
 import com.mechasystem.mapaintercambista.dto.request.RegisterUserRequest;
+import com.mechasystem.mapaintercambista.dto.response.AgenciaResponse;
 import com.mechasystem.mapaintercambista.dto.response.IntercambistaResponse;
 import com.mechasystem.mapaintercambista.dto.response.LoginResponse;
 import com.mechasystem.mapaintercambista.dto.response.RegisterUserResponse;
 import com.mechasystem.mapaintercambista.enums.Role;
 import com.mechasystem.mapaintercambista.exception.ConflictException;
-import com.mechasystem.mapaintercambista.model.Intercambista;
 import com.mechasystem.mapaintercambista.model.User;
-import com.mechasystem.mapaintercambista.repository.IntercambistaRepository;
 import com.mechasystem.mapaintercambista.repository.UserRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
     private final IntercambistaService intercambistaService;
+    private final AgenciaService agenciaService;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final TokenConfig tokenConfig;
 
     public UserService (UserRepository userRepository, PasswordEncoder passwordEncoder,
                         AuthenticationManager authenticationManager, TokenConfig tokenConfig,
-                        IntercambistaService intercambistaService) {
+                        IntercambistaService intercambistaService, AgenciaService agenciaService) {
         this.userRepository = userRepository;
         this.intercambistaService = intercambistaService;
+        this.agenciaService = agenciaService;
         this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
         this.tokenConfig = tokenConfig;
@@ -52,27 +52,42 @@ public class UserService {
     }
 
     @Transactional
-    public RegisterUserResponse registerUser(RegisterUserRequest request) {
-        if(userRepository.findUserByEmail(request.email()).isPresent()) {
-            throw new ConflictException("E-mail já está em uso");
-        }
+    public RegisterUserResponse registerIntercambista(RegisterUserRequest request) {
+        validarEmail(request.email());
 
-        User nUser = new User();
-        nUser.setRole(Role.ROLE_USER);
-        nUser.setEmail(request.email());
-        nUser.setPassword(passwordEncoder.encode(request.password()));
-
-        userRepository.save(nUser);
-
-        IntercambistaResponse intercambistaResponse = intercambistaService.saveIntercambista(request, nUser);
+        User u = registerUser(request.email(), request.password(), Role.ROLE_USER);
+        IntercambistaResponse intercambistaResponse = intercambistaService.saveIntercambista(request, u);
 
         return new RegisterUserResponse(
-                nUser.getEmail(),
+                u.getEmail(),
                 intercambistaResponse.username()
         );
     }
 
-    public RegisterUserResponse registerAgencia(RegisterUserRequest request) {
-        return null;
+    @Transactional
+    public AgenciaResponse registerAgencia(CreateAgenciaRequest request) {
+        validarEmail(request.email());
+
+        User u = registerUser(request.email(), request.password(), Role.ROLE_AGENCIA);
+
+        return agenciaService.saveAgencia(request, u);
     }
+
+    private User registerUser(String email, String password, Role r) {
+        User nUser = new User();
+        nUser.setRole(r);
+        nUser.setEmail(email);
+        nUser.setPassword(passwordEncoder.encode(password));
+
+        userRepository.save(nUser);
+
+        return nUser;
+    }
+
+    private void validarEmail (String email) {
+        if(userRepository.findUserByEmail(email).isPresent()) {
+            throw new ConflictException("E-mail já está em uso");
+        }
+    }
+
 }
