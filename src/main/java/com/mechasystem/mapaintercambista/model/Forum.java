a9f0601a0ef28f3e796b82ecac73741baeb134aa
Pablo Ferreira <emanuelpabloferreira1@gmail.com>
2026-05-17 20:31:46 -0300

    Modelo da tabela foruns

---
+++ src/main/java/com/mechasystem/mapaintercambista/model/Forum.java
@@ -0,0 +1,44 @@
+package com.mechasystem.mapaintercambista.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "foruns")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Forum {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_intercambista")
+    private Intercambista intercambista;
+
+    private String titulo;
+
+    @Builder.Default
+    private Integer likes = 0;
+
+    @Builder.Default
+    private Integer dislikes = 0;
+
+    @Column(name = "created_at")
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+
+
+}
